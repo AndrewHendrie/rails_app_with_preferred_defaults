b851cb05cd439f2094c0e2007db49203454ca745
Andrew Hendrie <hendrieandrew91@gmail.com>
2016-02-10 02:49:07 -0500

    updated sessions controller and application controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root 'page#index'
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+  get '/signin' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
 end
