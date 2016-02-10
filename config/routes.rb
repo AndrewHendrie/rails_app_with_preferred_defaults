765001e464a87f325b9e234731dea90c6fdb9d08
Andrew Hendrie <hendrieandrew91@gmail.com>
2016-02-10 03:26:32 -0500

    added sign in, sign up, and sign out links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'page/index'
   root 'page#index'
-  get '/signup' => 'users#new'
+  get '/signup' => 'users#new', as: :sign_up
   post '/users' => 'users#create'
-  get '/signin' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get 'logout' => 'sessions#destroy'
+  get '/signin' => 'sessions#new', as: :sign_in
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
 end
