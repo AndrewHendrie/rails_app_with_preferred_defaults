9da73bd7ed424a9a48d18a1629d3f51d53548f80
AndrewHendrie <hendrieandrew91@gmail.com>
2016-02-23 08:52:21 -0500

    removed user model from default app

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
   get 'page/index'
   root 'page#index'
-  get '/signup' => 'users#new', as: :sign_up
-  post '/users' => 'users#create'
-  get '/signin' => 'sessions#new', as: :sign_in
-  post '/signin' => 'sessions#create'
-  get '/signout' => 'sessions#destroy'
 end
